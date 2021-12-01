dc5ee5b6e7b0523e7ea0c19743ebdca68df9dbda
Arthur Sampaio <arthur.correia@vtex.com.br>
2021-12-01 12:33:17 -0300

    Add vtex-test-tools. Export OrderPayment as before

--- react/OrderPayment.tsx
+++ react/OrderPayment.tsx
@@ -1,56 +1,6 @@
-import React, {
-  createContext,
-  ReactNode,
-  useContext,
-  useCallback,
-  useMemo,
-  useState,
-} from 'react'
-import { useMutation } from 'react-apollo'
-import MutationUpdateOrderFormPayment from 'vtex.checkout-resources/MutationUpdateOrderFormPayment'
-import { OrderQueue, OrderForm } from 'vtex.order-manager'
-import {
-  OrderForm as CheckoutOrderForm,
-  PaymentDataInput,
-  PaymentSystem,
-  InstallmentOption,
-  AvailableAccount,
-  Payment,
-  PaymentInput,
-} from 'vtex.checkout-graphql'
-
 import {
   OrderPaymentProvider,
   useOrderPayment,
 } from './components/OrderPayments'
-
-const { useOrderForm } = OrderForm
-const { useOrderQueue, useQueueStatus, QueueStatus } = OrderQueue
-
-interface UpdateOrderFormPaymentMutation {
-  updateOrderFormPayment: CheckoutOrderForm
-}
-
-interface UpdateOrderFormPaymentMutationVariables {
-  paymentData: PaymentDataInput
-}
-
-interface Context {
-  setPaymentField: (
-    paymentField: Partial<PaymentInput>
-  ) => Promise<{ success: boolean }>
-  paymentSystems: PaymentSystem[]
-  availableAccounts: AvailableAccount[]
-  installmentOptions: InstallmentOption[]
-  payment: Payment
-  referenceValue: number
-  value: number
-  interestValue: number
-  cardLastDigits: string
-  setCardLastDigits: (cardDigits: string) => void
-  paymentsValid: boolean
-  setCardFormFilled: (filled: boolean) => void
-  isFreePurchase: boolean
-}
 
 export default { useOrderPayment, OrderPaymentProvider }
---
+++ react/__tests__/createOrderPaymentProvider.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@vtex/test-tools/react'
+
+import HelloWorld from './HelloWorld'
+import { createOrderPaymentProvider } from '../components/createOrderPaymentProvider'
+
+test('should render the Hello!', () => {
+  const a = null
+
+  expect(a).toBeNull()
+})
